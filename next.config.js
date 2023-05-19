6c592c25b25126211cc5045c7aafaf26b3d20e38
trent <15133853+trentbaker@users.noreply.github.com>
2023-05-19 14:37:32 -0600

    some slashes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? "trentbaker.github.io" : "",
+  assetPrefix: isProd ? "/trentbaker.github.io/" : "",
   images: {
     unoptimized: true,
   },
