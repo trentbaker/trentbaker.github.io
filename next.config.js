e0f4c6bae8637476985047b6aae3d36770559f81
trent <15133853+trentbaker@users.noreply.github.com>
2023-05-19 14:40:27 -0600

    export static pls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   assetPrefix: isProd ? "/trentbaker.github.io/" : "",
   images: {
     unoptimized: true,
