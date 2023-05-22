956a0b4172adcde07c902462084ccc77fd06a1d1
trent <15133853+trentbaker@users.noreply.github.com>
2023-05-22 17:51:28 -0600

    start with straight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,65 +3,94 @@
 interface Player {
   name: string;
   pointHistory: number[];
+  score: number;
 }
 
 interface CribbageBoardProps {
-  game: Player[];
+  players: Player[];
+}
+
+interface Coodinate {
+  x: number;
+  y: number;
 }
 
 const CribbageBoard = (props: CribbageBoardProps) => {
-  const boardHeight = 1000;
-  const boardWidth = 500;
-  const padding = 20;
-  const laneWidth = 20;
-  const topCurveY = (boardWidth - 2 * padding) / 2 + padding;
-  const bottomCurveY =boardHeight  - boardWidth / 2 - (props.game.length * laneWidth) / 2
+  const { players } = props;
 
-  console.log(
-    JSON.stringify(
-      {
-        boardHeight,
-        boardWidth,
-        padding,
-        laneWidth,
-        topCurveY,
-        bottomCurveY,
-      },
-      null,
-      2
-    )
+  const svgWidth = 500;
+  const svgHeight = 1000;
+  const padding = 25;
+
+  const boardWidth = svgWidth - padding * 2;
+  const boardHeight = svgHeight - padding * 2;
+
+  const holeSpacing = boardHeight / 120;
+  const laneSpacing = boardWidth / (players.length + 1);
+
+  const laneCoordinates: Coodinate[][] = players.map((_, playerIndex) =>
+    Array.from({ length: 120 }, (_, holeIndex) => {
+      return {
+        x: (1 + playerIndex) * laneSpacing + padding,
+        y: (1 + holeIndex) * holeSpacing + padding,
+      };
+    })
   );
 
-  const route = (offset: number) => {
-    const actual =
-      boardWidth / 2 + offset - (props.game.length * laneWidth) / 2;
-
-    return `
-    M ${padding + offset} ${bottomCurveY}
-    L ${padding + offset} ${topCurveY}
-    A 1 1 0 0 1 ${boardWidth - padding - offset} ${topCurveY}
-    L ${boardWidth - padding - offset} ${bottomCurveY}
-    A 1 1 0 0 1 ${actual} ${bottomCurveY}
-    L ${actual} ${topCurveY}
-  `;
-  };
-
-  const street = (offset: number, streetWidth: number) => {
-    return `${route(offset)}${route(streetWidth + offset)}`;
-  };
-
-  const allLanes = (laneCount: number, laneWidth: number) => {
-    let out = "";
-    for (let i = 0; i < laneCount; i++) {
-      out += street(i * laneWidth, laneWidth);
+  const playerPegCoordinates: Coodinate[][] = players.map(
+    (player, playerIndex) => {
+      return player.pointHistory
+        .slice(player.pointHistory.length - 2)
+        .map((peg, pegIndex) => {
+          return laneCoordinates[playerIndex][player.score - pegIndex * peg];
+        });
     }
-    return out;
-  };
+  );
 
   return (
-    <svg className="bg-base-content" width={boardWidth} height={boardHeight}>
-      <g stroke="black" fill="none" strokeWidth="5px">
-        <path d={allLanes(props.game.length, laneWidth)}></path>
+    <svg className="bg-base-content" width={svgWidth} height={svgHeight}>
+      <g
+        // drawing all holes
+        stroke="black"
+        fill="none"
+        strokeWidth="2px"
+        width={boardWidth}
+        height={boardHeight}
+      >
+        {laneCoordinates.map((lane) =>
+          lane.map((hole) => {
+            return (
+              <circle
+                key={JSON.stringify(hole)}
+                cx={hole.x}
+                cy={hole.y}
+                r="1px"
+              ></circle>
+            );
+          })
+        )}
+      </g>
+
+      <g
+        // drawing player pegs
+        stroke="yellow"
+        fill="none"
+        strokeWidth="2px"
+        width={boardWidth}
+        height={boardHeight}
+      >
+        {playerPegCoordinates.map((playerPegs) =>
+          playerPegs.map((peg) => {
+            return (
+              <circle
+                key={JSON.stringify(peg)}
+                cx={peg.x}
+                cy={peg.y}
+                r="1px"
+              ></circle>
+            );
+          })
+        )}
       </g>
     </svg>
   );
@@ -69,30 +98,30 @@ const CribbageBoard = (props: CribbageBoardProps) => {
 
 export default function CribbageScoreCard() {
   const players: Player[] = [
-    { name: "trent", pointHistory: [1, 1, 3] },
-    { name: "bonney", pointHistory: [2, 2] },
-    { name: "shy", pointHistory: [2, 2] },
-    { name: "garrett", pointHistory: [1, 1, 1, 1] },
+    { name: "trent", pointHistory: [1, 1, 3], score: 40 },
+    { name: "bonney", pointHistory: [2, 2], score: 40 },
+    { name: "shy", pointHistory: [2, 2], score: 40 },
+    { name: "garrett", pointHistory: [1, 1, 1, 1], score: 40 },
   ];
   return (
-    <div className="flex items-center h-screen place-content-around">
-      <CribbageBoard game={players.slice(0, 1)}></CribbageBoard>
-      <CribbageBoard game={players.slice(0, 2)}></CribbageBoard>
-      <CribbageBoard game={players.slice(0, 3)}></CribbageBoard>
-      <CribbageBoard game={players.slice(0, 4)}></CribbageBoard>
-
-      {/* {players.map((player) => {
-        return (
-          <div className="stats shadow bg-base-content" key={player.name}>
-            <div className="stat">
-              <div className="stat-title text-base-100">{player.name}</div>
-              <div className="stat-value text-base-100">
-                {player.pointHistory.reduce((a, b) => a + b)}
+    <div className="flex flex-col h-screen place-content-around">
+      <div className="flex place-content-around">
+        <CribbageBoard players={players.slice(0, 4)}></CribbageBoard>
+      </div>
+      <div className="flex justify-center items-center">
+        {players.map((player) => {
+          return (
+            <div className="stats shadow bg-base-content" key={player.name}>
+              <div className="stat">
+                <div className="stat-title text-base-100">{player.name}</div>
+                <div className="stat-value text-base-100">
+                  {player.pointHistory.reduce((a, b) => a + b)}
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })} */}
+          );
+        })}
+      </div>
     </div>
   );
 }
