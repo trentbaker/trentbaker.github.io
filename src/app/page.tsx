5631aebcb3209671e354c164b0d8349497d8c996
trent <15133853+trentbaker@users.noreply.github.com>
2023-05-29 13:45:01 -0600

    single player keeping score

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { makeDraggable } from "@/lib/makeDraggable";
 import { randomBytes } from "crypto";
 import { useRef, useState } from "react";
 
@@ -13,17 +12,16 @@ interface CribbageBoardConfig {
   svgHeight: number;
   svgWidth: number;
   holeCount: number;
+  players: Player[];
 }
 
 const CribbageBoard = (config: CribbageBoardConfig) => {
-  const { svgHeight, svgWidth, holeCount } = config;
+  const { svgHeight, svgWidth, holeCount, players } = config;
 
   const pathRoute = `
     M 100 50
     L 100 750
     `;
-    // A 1 1 0 0 0 200 400
-    // A 1 1 0 0 0 200 50
 
   const [pathElement, setPathElement] = useState<SVGPathElement>();
 
@@ -32,6 +30,16 @@ const CribbageBoard = (config: CribbageBoardConfig) => {
   const holeCoordinates = Array.from({ length: holeCount }).map((_, i) => {
     const distance = (i + 1) * holeSpacing;
     return pathElement?.getPointAtLength(distance);
+  });
+
+  const playerPegs = players.flatMap((player) => {
+    const score = player.pointHistory.reduce((a, b) => a + b);
+    const out = [
+      holeCoordinates[score],
+      holeCoordinates[score - player.pointHistory.slice(-1)[0]],
+    ];
+
+    return out;
   });
 
   return (
@@ -45,30 +53,51 @@ const CribbageBoard = (config: CribbageBoardConfig) => {
         fill="none"
         strokeWidth="15px"
       ></path>
-      {holeCoordinates.map((hole) => {
-        return (
-          <circle
-            stroke="blue"
-            fill="none"
-            strokeWidth="2px"
-            r="10px"
-            key={randomBytes(6).toString()}
-            cx={hole?.x}
-            cy={hole?.y}
-          ></circle>
-        );
-      })}
+      {holeCoordinates.map((hole) => (
+        <circle
+          stroke="blue"
+          fill="none"
+          strokeWidth="2px"
+          r="10px"
+          key={randomBytes(6).toString()}
+          cx={hole?.x}
+          cy={hole?.y}
+        ></circle>
+      ))}
+      {playerPegs.map((peg, i) => (
+        <circle
+          key={randomBytes(4).toString()}
+          cx={peg?.x}
+          cy={peg?.y}
+          stroke="yellow"
+          strokeWidth="2px"
+          r="4px"
+        ></circle>
+      ))}
     </svg>
   );
 };
 
 export default function CribbageScoreCard() {
-  const players: Player[] = [
+  const [players, setPlayers] = useState([
     { name: "trent", pointHistory: [1, 1, 3] },
-    { name: "bonney", pointHistory: [2, 2] },
-    { name: "shy", pointHistory: [2, 2] },
-    { name: "garrett", pointHistory: [1, 1, 1, 1] },
-  ];
+    // { name: "bonney", pointHistory: [2, 2] },
+    // { name: "shy", pointHistory: [2, 2] },
+    // { name: "garrett", pointHistory: [1, 1, 1, 1] },
+  ]);
+
+  const updatePlayerPoints = (name: string, score: number) =>
+    setPlayers(
+      players.map((p) => {
+        if (name == p.name)
+          return { ...p, pointHistory: [...p.pointHistory, score] };
+        else return p;
+      })
+    );
+
+  const [selectedPlayerName, setSelectedPlayerName] = useState(players[0].name);
+  const pointRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="flex flex-col h-screen place-content-around">
       <div className="flex place-content-around">
@@ -76,12 +105,13 @@ export default function CribbageScoreCard() {
           svgHeight={800}
           svgWidth={400}
           holeCount={10}
+          players={players}
         ></CribbageBoard>
       </div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center˝">
         {players.map((player) => {
           return (
-            <div className="stats shadow bg-base-content" key={player.name}>
+            <div key={player.name} className="stats bg-base-content">
               <div className="stat">
                 <div className="stat-title text-base-100">{player.name}</div>
                 <div className="stat-value text-base-100">
@@ -91,6 +121,39 @@ export default function CribbageScoreCard() {
             </div>
           );
         })}
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="input-group">
+          <select
+            className="select select-bordered"
+            value={selectedPlayerName}
+            onChange={(e) => setSelectedPlayerName(e.target.value)}
+          >
+            {players.map((player) => {
+              return (
+                <option key={player.name} value={player.name}>
+                  {player.name}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            className="input input-bordered grow-0"
+            type="number"
+            ref={pointRef}
+          ></input>
+          <a
+            className="btn"
+            onClick={() => {
+              updatePlayerPoints(
+                selectedPlayerName,
+                Number(pointRef.current?.value || 0)
+              );
+            }}
+          >
+            Score
+          </a>
+        </div>
       </div>
     </div>
   );
